e64d9fee878bb54332b827142fb0c4eea0990e39
Nataly Dugasova <dugasova@gmail.com>
2026-06-16 09:13:30 +0000

    refactor: optimize cart selectors using createSelector for memoization

--- src/store/features/cartList/selectors.ts
+++ src/store/features/cartList/selectors.ts
@@ -1,9 +1,14 @@
+import { createSelector } from "@reduxjs/toolkit";
 import type { RootState } from "../../store";
 
 export const selectCartList = (state: RootState) => state.cartList.cartList;
 
-export const selectTotalPrice = (state: RootState) =>
-  state.cartList.cartList.reduce((acc, i) => acc + (i.discountPrice ?? i.price) * i.totalQuantity, 0);
+export const selectTotalPrice = createSelector(
+  selectCartList,
+  (cartList) => cartList.reduce((acc, i) => acc + (i.discountPrice ?? i.price) * i.totalQuantity, 0)
+);
 
-export const selectTotalQuantity = (state: RootState) =>
-  state.cartList.cartList.reduce((acc, i) => acc + i.totalQuantity, 0);
+export const selectTotalQuantity = createSelector(
+  selectCartList,
+  (cartList) => cartList.reduce((acc, i) => acc + i.totalQuantity, 0)
+);
